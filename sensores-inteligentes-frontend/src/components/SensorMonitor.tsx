f3fb93a1e1a9edcc3576420a7991f5bdd5ef5505
Gabriel Assis <43077215+bielworm@users.noreply.github.com>
2024-11-05 18:33:46 -0300

    fix: add env

--- sensores-inteligentes-frontend/src/components/SensorMonitor.tsx
+++ sensores-inteligentes-frontend/src/components/SensorMonitor.tsx
@@ -3,15 +3,15 @@ import { initializeApp } from 'firebase/app'
 import { getDatabase, ref, onValue } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBXDyn_OUk3Q8C4efUKWXZzCc_e_3f7Iw4',
-  authDomain: 'sensores-refrigeradores.firebaseapp.com',
-  databaseURL: 'https://sensores-refrigeradores-default-rtdb.firebaseio.com',
-  projectId: 'sensores-refrigeradores',
-  storageBucket: 'sensores-refrigeradores.appspot.com',
-  messagingSenderId: '76178323841',
-  appId: '1:76178323841:web:7470185be1cd9c4af8d080',
-  measurementId: 'G-35D20V0VVM',
-}
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
--- sensores-inteligentes-frontend/src/pages/index.tsx
+++ sensores-inteligentes-frontend/src/pages/index.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import { LoginForm } from '../components/Forms/LoginForm'
 import Container from '../components/Partials/Container'
 import SensorMonitor from '../components/SensorMonitor'
+import { Icon } from '@iconify/react'
 
 const Home: NextPage = () => {
 
@@ -11,7 +12,15 @@ const Home: NextPage = () => {
     <div className="bg-gradient-to-tl from-[#141414] to-[#272727] w-full h-screen flex items-center justify-center bg-cover">
       <Container>
         <div className="shadow-sm shadow-zinc-200 p-8 w-full rounded-lg backdrop-blur">
-          <h2 className="font-thin underline text-white mb-4 text-center">Sensores - Refrigeradores</h2>
+          <div className="flex flex-row justify-center items-start gap-2">
+            <h2 className="font-thin underline text-white mb-4 text-center">
+              Sensores - Refrigeradores
+            </h2>
+            <Icon
+              icon="iconoir:temperature-low"
+              className="text-white text-3xl"
+            />
+          </div>
           <SensorMonitor />
         </div>
       </Container>
